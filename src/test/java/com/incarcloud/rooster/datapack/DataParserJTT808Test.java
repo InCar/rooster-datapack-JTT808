7fe2ca5bc6f4683981ee8159449bb464388b0169
aaric <vipaaric@gmail.com>
2017-08-22 10:39:06 +0800

    init

--- src/test/java/com/incarcloud/rooster/datapack/DataParserJTT808Test.java
+++ src/test/java/com/incarcloud/rooster/datapack/DataParserJTT808Test.java
@@ -1,5 +1,14 @@
 package com.incarcloud.rooster.datapack;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.util.ReferenceCountUtil;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * DataParserJTT808Test
  *
@@ -7,4 +16,50 @@ package com.incarcloud.rooster.datapack;
  * @since 2.0
  */
 public class DataParserJTT808Test {
+
+    /**
+     * Logger
+     */
+    private static final Logger logger = LoggerFactory.getLogger(DataParserJTT808Test.class);
+
+    /**
+     * Data
+     */
+    private ByteBuf buffer;
+
+    @Before
+    public void begin() {
+        byte[] data = {};
+        buffer = Unpooled.wrappedBuffer(data);
+    }
+
+    @After
+    public void end() {
+        ReferenceCountUtil.release(buffer);
+    }
+
+    @Test
+    public void testExtract() {
+
+    }
+
+    @Test
+    public void testCreateResponse() {
+
+    }
+
+    @Test
+    public void testDestroyResponse() {
+
+    }
+
+    @Test
+    public void testExtractBody() {
+
+    }
+
+    @Test
+    public void testGetMetaData() {
+
+    }
 }
