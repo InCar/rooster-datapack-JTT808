cbe8c965b646bef6af9d267118c66f56afb80a8d
aaric <vipaaric@gmail.com>
2017-08-23 18:05:42 +0800

    接收消息时：转义还原——>验证校验码——>解析消息

--- src/main/java/com/incarcloud/rooster/datapack/DataParserJTT808.java
+++ src/main/java/com/incarcloud/rooster/datapack/DataParserJTT808.java
@@ -197,12 +197,305 @@ public class DataParserJTT808 implements IDataParser {
                 // 初始化ByteBuf
                 buffer = Unpooled.wrappedBuffer(dataPackBytes);
 
+                /* 标识位 */
                 // 跳过标识位(0x7e)
                 buffer.readBytes(1);
 
-                // 消息ID
+                /* 消息头 */
+                // 1.消息ID
                 int msgId = JTT808DataPackUtil.readWord(buffer);
-                System.out.println(msgId);
+                System.out.println("msgId: " + msgId);
+                // 2.消息体属性
+                int msgProps = JTT808DataPackUtil.readWord(buffer);
+                // 2.1 消息体长度
+                int msgLength = msgProps & 0x01FF;
+                System.out.println("msgLength: " + msgLength);
+                // 2.2 数据加密方式
+                int msgEncryptMode = (msgProps >> 10) & 0x0007;
+                System.out.println("msgEncryptMode: " + msgEncryptMode);
+                switch (msgEncryptMode) {
+                    case 0:
+                        // 消息体不加密
+                        System.out.println("--消息体不加密");
+                        break;
+                    case 1:
+                        // 第 10 位为 1，表示消息体经过 RSA 算法加密
+                        System.out.println("--RSA 算法加密");
+                }
+                // 2.3 分包
+                int msgSubPack = (msgProps >> 13) & 0x0001;
+                System.out.println("msgSubPack: " + msgSubPack);
+
+
+                /* 消息体 */
+                switch (msgId) {
+                    case 0x001:
+                        /* 终端通用应答 */
+                        System.out.println("## 0x001 - 终端通用应答");
+                        break;
+                    case 0x8001:
+                        /* 平台通用应答 */
+                        System.out.println("## 0x8001 - 平台通用应答");
+                        break;
+                    case 0x0002:
+                        /* 终端心跳 */
+                        System.out.println("## 0x0002 - 终端心跳");
+                        break;
+                    case 0x8003:
+                        /* 补传分包请求 */
+                        System.out.println("## 0x8003 - 补传分包请求");
+                        break;
+                    case 0x0100:
+                        /* 终端注册 */
+                        System.out.println("## 0x0100 - 终端注册");
+                        break;
+                    case 0x8100:
+                        /* 终端注册应答 */
+                        System.out.println("## 0x8100 - 终端注册应答");
+                        break;
+                    case 0x0003:
+                        /* 终端注销 */
+                        System.out.println("## 0x0003 - 终端注销");
+                        break;
+                    case 0x0102:
+                        /* 终端鉴权 */
+                        System.out.println("## 0x0102 - 终端鉴权");
+                        break;
+                    case 0x8103:
+                        /* 设置终端参数 */
+                        System.out.println("## 0x8103 - 设置终端参数");
+                        break;
+                    case 0x8104:
+                        /* 查询终端参数 */
+                        System.out.println("## 0x8104 - 查询终端参数");
+                        break;
+                    case 0x0104:
+                        /* 查询终端参数应答 */
+                        System.out.println("## 0x0104 - 查询终端参数应答");
+                        break;
+                    case 0x8105:
+                        /* 终端控制 */
+                        System.out.println("## 0x8105 - 终端控制");
+                        break;
+                    case 0x8106:
+                        /* 查询指定终端参数 */
+                        System.out.println("## 0x8106 - 查询指定终端参数");
+                        break;
+                    case 0x8107:
+                        /* 查询终端属性 */
+                        System.out.println("## 0x8107 - 查询终端属性");
+                        break;
+                    case 0x0107:
+                        /* 查询终端属性应答 */
+                        System.out.println("## 0x0107 - 查询终端属性应答");
+                        break;
+                    case 0x8108:
+                        /* 下发终端升级包 */
+                        System.out.println("## 0x8108 - 下发终端升级包");
+                        break;
+                    case 0x0108:
+                        /* 终端升级结果通知 */
+                        System.out.println("## 0x0108 - 终端升级结果通知");
+                        break;
+                    case 0x0200:
+                        /* 位置信息汇报 */
+                        System.out.println("## 0x0200 - 位置信息汇报");
+                        break;
+                    case 0x8201:
+                        /* 位置信息查询 */
+                        System.out.println("## 0x8201 - 位置信息查询");
+                        break;
+                    case 0x0201:
+                        /* 位置信息查询应答 */
+                        System.out.println("## 0x0201 - 位置信息查询应答");
+                        break;
+                    case 0x8202:
+                        /* 临时位置跟踪控制 */
+                        System.out.println("## 0x8202 - 临时位置跟踪控制");
+                        break;
+                    case 0x8203:
+                        /* 人工确认报警消息 */
+                        System.out.println("## 0x8203 - 人工确认报警消息");
+                        break;
+                    case 0x8300:
+                        /* 文本信息下发 */
+                        System.out.println("## 0x8300 - 文本信息下发");
+                        break;
+                    case 0x8301:
+                        /* 事件设置 */
+                        System.out.println("## 0x8301 - 事件设置");
+                        break;
+                    case 0x0301:
+                        /* 事件报告 */
+                        System.out.println("## 0x0301 - 事件报告");
+                        break;
+                    case 0x8302:
+                        /* 提问下发 */
+                        System.out.println("## 0x8302 - 提问下发");
+                        break;
+                    case 0x0302:
+                        /* 提问应答 */
+                        System.out.println("## 0x0302 - 提问应答");
+                        break;
+                    case 0x8303:
+                        /* 信息点播菜单设置 */
+                        System.out.println("## 0x8303 - 信息点播菜单设置");
+                        break;
+                    case 0x0303:
+                        /* 信息点播/取消 */
+                        System.out.println("## 0x0303 - 信息点播/取消");
+                        break;
+                    case 0x8304:
+                        /* 信息服务 */
+                        System.out.println("## 0x8304 - 信息服务");
+                        break;
+                    case 0x8400:
+                        /* 电话回拨 */
+                        System.out.println("## 0x8400 - 电话回拨");
+                        break;
+                    case 0x8401:
+                        /* 设置电话本 */
+                        System.out.println("## 0x8401 - 设置电话本");
+                        break;
+                    case 0x8500:
+                        /* 车辆控制 */
+                        System.out.println("## 0x8500 - 车辆控制");
+                        break;
+                    case 0x0500:
+                        /* 车辆控制应答 */
+                        System.out.println("## 0x0500 - 车辆控制应答");
+                        break;
+                    case 0x8600:
+                        /* 设置圆形区域 */
+                        System.out.println("## 0x8600 - 设置圆形区域");
+                        break;
+                    case 0x8601:
+                        /* 删除圆形区域 */
+                        System.out.println("## 0x8601 - 删除圆形区域");
+                        break;
+                    case 0x8602:
+                        /* 设置矩形区域 */
+                        System.out.println("## 0x8602 - 设置矩形区域");
+                        break;
+                    case 0x8603:
+                        /* 删除矩形区域 */
+                        System.out.println("## 0x8603 - 删除矩形区域");
+                        break;
+                    case 0x8604:
+                        /* 设置多边形区域 */
+                        System.out.println("## 0x8604 - 设置多边形区域");
+                        break;
+                    case 0x8605:
+                        /* 删除多边形区域 */
+                        System.out.println("## 0x8605 - 删除多边形区域");
+                        break;
+                    case 0x8606:
+                        /* 设置路线 */
+                        System.out.println("## 0x8606 - 设置路线");
+                        break;
+                    case 0x8607:
+                        /* 删除路线 */
+                        System.out.println("## 0x8607 - 删除路线");
+                        break;
+                    case 0x8700:
+                        /* 行驶记录仪数据采集命令 */
+                        System.out.println("## 0x8700 - 行驶记录仪数据采集命令");
+                        break;
+                    case 0x0700:
+                        /* 行驶记录仪数据上传 */
+                        System.out.println("## 0x0700 - 行驶记录仪数据上传");
+                        break;
+                    case 0x8701:
+                        /* 行驶记录仪参数下传命令 */
+                        System.out.println("## 0x8701 - 行驶记录仪参数下传命令");
+                        break;
+                    case 0x0701:
+                        /* 电子运单上报 */
+                        System.out.println("## 0x0701 - 电子运单上报");
+                        break;
+                    case 0x0702:
+                        /* 驾驶员身份信息采集上报 */
+                        System.out.println("## 0x0702 - 驾驶员身份信息采集上报");
+                        break;
+                    case 0x8702:
+                        /* 上报驾驶员身份信息请求 */
+                        System.out.println("## 0x8702 - 上报驾驶员身份信息请求");
+                        break;
+                    case 0x0704:
+                        /* 定位数据批量上传 */
+                        System.out.println("## 0x0704 - 定位数据批量上传");
+                        break;
+                    case 0x0705:
+                        /* CAN 总线数据上传 */
+                        System.out.println("## 0x0705 - CAN 总线数据上传");
+                        break;
+                    case 0x0800:
+                        /* 多媒体事件信息上传 */
+                        System.out.println("## 0x0800 - 多媒体事件信息上传");
+                        break;
+                    case 0x0801:
+                        /* 多媒体数据上传 */
+                        System.out.println("## 0x0801 - 多媒体数据上传");
+                        break;
+                    case 0x8800:
+                        /* 多媒体数据上传应答 */
+                        System.out.println("## 0x8800 - 多媒体数据上传应答");
+                        break;
+                    case 0x8801:
+                        /* 摄像头立即拍摄命令 */
+                        System.out.println("## 0x8801 - 摄像头立即拍摄命令");
+                        break;
+                    case 0x0805:
+                        /* 摄像头立即拍摄命令应答 */
+                        System.out.println("## 0x0805 - 摄像头立即拍摄命令应答");
+                        break;
+                    case 0x8802:
+                        /* 存储多媒体数据检索 */
+                        System.out.println("## 0x0802 - 存储多媒体数据检索");
+                        break;
+                    case 0x0802:
+                        /* 存储多媒体数据检索应答 */
+                        System.out.println("## 0x0802 - 存储多媒体数据检索应答");
+                        break;
+                    case 0x8803:
+                        /* 存储多媒体数据上传 */
+                        System.out.println("## 0x8803 - 存储多媒体数据上传");
+                        break;
+                    case 0x8804:
+                        /* 录音开始命令 */
+                        System.out.println("## 0x8804 - 录音开始命令");
+                        break;
+                    case 0x8805:
+                        /* 单条存储多媒体数据检索上传命令 */
+                        System.out.println("## 0x8805 - 单条存储多媒体数据检索上传命令");
+                        break;
+                    case 0x8900:
+                        /* 数据下行透传 */
+                        System.out.println("## 0x8900 - 数据下行透传");
+                        break;
+                    case 0x0900:
+                        /* 数据上行透传 */
+                        System.out.println("## 0x0900 - 数据上行透传");
+                        break;
+                    case 0x0901:
+                        /* 数据压缩上报 */
+                        System.out.println("## 0x0901 - 数据压缩上报");
+                        break;
+                    case 0x8A00:
+                        /* 平台 RSA 公钥 */
+                        System.out.println("## 0x8A00 - 平台 RSA 公钥");
+                        break;
+                    case 0x0A00:
+                        /* 终端 RSA 公钥 */
+                        System.out.println("## 0x0A00 - 终端 RSA 公钥");
+                        break;
+                    default:
+                        /**
+                         * 0x8F00~0x8FFF 平台下行消息保留
+                         * 0x0F00~0x0FFF 终端上行消息保留
+                         */
+                        System.out.println("## msgId(" + msgId + ") can't be parsed.");
+                }
 
 
             } catch (Exception e) {
